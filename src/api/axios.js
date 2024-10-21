7af1004520f0f93a90a5b3d75ea7b48ba509f981
J P <josessogamer@outlook.es>
2024-10-21 00:23:11 -0400

    correcionesfront2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const URL_API = import.meta.env.VITE_URL_API || 'https://backend-ydvu.onrender.com';
+export const URL_API = import.meta.env.VITE_URL_API || 'https://backend-jt2r.onrender.com';
 
 const instance = axios.create({
     baseURL: URL_API,
